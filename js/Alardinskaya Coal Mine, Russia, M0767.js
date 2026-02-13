7250d7e14f4aa965fdf7d8046e06cae7014bcec3
dominicnicholas <Dominic.s.nicholas@gmail.com>
2026-02-13 10:46:03 -0500

    Update for release Coal Mine Boundaries and Methane Sources - version 1.0.1

--- js/Alardinskaya Coal Mine, Russia, M0767.js
+++ js/Alardinskaya Coal Mine, Russia, M0767.js
@@ -925,7 +925,7 @@
     var bounds = L.latLngBounds(L.latLng(53.393837, 87.273332), L.latLng(53.456994, 87.438631));                        
     var googleStreet =  L.tileLayer('https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}', {maxZoom: 18, attribution: '&copy; Google Maps'})
     var googleHybrid =  L.tileLayer('https://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}', {maxZoom: 18, attribution: '&copy; Google Maps'})
-    var  map = L.map('map', {layers: [googleStreet, googleHybrid]}).fitBounds(bounds, { animate: false }}
+    var  map = L.map('map', {layers: [googleStreet, googleHybrid]}).fitBounds(bounds, { animate: false })
 
     // no difference...
     //var map = L.map('map', {layers: [googleStreet, googleHybrid]}).fitBounds([ [53.393837, 87.273332], [53.456994, 87.438631] ])
