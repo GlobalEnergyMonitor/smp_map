ff9f76210220f86408c1441a60f185f3734f25c9
dominicnicholas <Dominic.s.nicholas@gmail.com>
2026-02-13 11:56:14 -0500

    Update for release Coal Mine Boundaries and Methane Sources - version 1.0.1

--- js/Alardinskaya Coal Mine, Russia, M0767.js
+++ js/Alardinskaya Coal Mine, Russia, M0767.js
@@ -925,9 +925,9 @@
     var bounds = L.latLngBounds(L.latLng(53.393837, 87.273332), L.latLng(53.456994, 87.438631));                        
     var googleStreet =  L.tileLayer('https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}', {maxZoom: 18, attribution: '&copy; Google Maps'})
     var googleHybrid =  L.tileLayer('https://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}', {maxZoom: 18, attribution: '&copy; Google Maps'})
-    var  map = L.map('map', {layers: [googleStreet, googleHybrid]}).fitBounds(bounds)
+    //var  map = L.map('map', {layers: [googleStreet, googleHybrid]}).fitBounds(bounds)
 
-    //const map = L.map('map', {layers: [googleStreet, googleHybrid]}).setView([53.4254155, 87.3559815], 11); // this works consistently - i.e no fitBounds call
+    const map = L.map('map', {layers: [googleStreet, googleHybrid]}).setView([53.4254155, 87.3559815], 11); // this works consistently - i.e no fitBounds call
     //const map = L.map('map', {layers: [googleStreet, googleHybrid]}).setView(bounds.getCenter(), 10); // doesn't work
 
 
@@ -957,8 +957,7 @@
 
 
 const myDiv = document.getElementById('map');
-myDiv.style.width = "600px";
-myDiv.style.height = "500px";
+//myDiv.style.width = "600px"; myDiv.style.height = "500px"; // this doesn't always work
 const width = myDiv.offsetWidth;
 const height = myDiv.offsetHeight;
 console.log(`Width: ${width}px, Height: ${height}px`);
