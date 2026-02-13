907b0f12b2abd9b7eb86fa87ca47953efe4677b5
dominicnicholas <Dominic.s.nicholas@gmail.com>
2026-02-13 10:45:20 -0500

    Update for release Coal Mine Boundaries and Methane Sources - version 1.0.1

--- js/Alardinskaya Coal Mine, Russia, M0767.js
+++ js/Alardinskaya Coal Mine, Russia, M0767.js
@@ -925,7 +925,7 @@
     var bounds = L.latLngBounds(L.latLng(53.393837, 87.273332), L.latLng(53.456994, 87.438631));                        
     var googleStreet =  L.tileLayer('https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}', {maxZoom: 18, attribution: '&copy; Google Maps'})
     var googleHybrid =  L.tileLayer('https://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}', {maxZoom: 18, attribution: '&copy; Google Maps'})
-    var  map = L.map('map', {layers: [googleStreet, googleHybrid]}).fitBounds(bounds)
+    var  map = L.map('map', {layers: [googleStreet, googleHybrid]}).fitBounds(bounds, { animate: false }}
 
     // no difference...
     //var map = L.map('map', {layers: [googleStreet, googleHybrid]}).fitBounds([ [53.393837, 87.273332], [53.456994, 87.438631] ])
@@ -959,5 +959,5 @@
 	GEMMine.bindPopup('Alardinskaya Coal Mine Operating mine');
 	GEMMine.bindTooltip('Alardinskaya Coal Mine Operating mine', { permanent: true, direction: 'right'});
 
-    //map.invalidateSize();
-    map.whenReady(() => { setTimeout(() => { map.invalidateSize(); }, 0); });
+    //map.invalidateSize(); // doesn't make diff
+    //map.whenReady(() => { setTimeout(() => { map.invalidateSize(); }, 0); }); // also doesn't make diff
